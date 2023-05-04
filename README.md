f2dfdcc2eaec87e21a5d542c1addaf9d93c2f376
Lee Jongyoung <leejongyoung98@inha.edu>
2023-05-05 03:32:38 +0900

    Add dotenv to load username and password from .env file. Fix an error in main.py.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# INHA iClass Login Checker
+# INHA Login Checker
 
-이 프로젝트는 파이썬을 사용하여 인하대학교 iClass 사이트에 로그인을 확인하는 스크립트입니다.
+이 프로젝트는 파이썬을 사용하여 인하대학교 사이트에 로그인을 확인하고 이름과 학번을 가져오는 스크립트입니다.
 
 ## 시작하기 전에
 
@@ -16,7 +16,7 @@ INHA_PASSWORD=your_password
 2. 필요한 패키지를 설치하세요. 터미널에서 다음 명령어를 실행하세요:
 
 ``` sh
-pip install python-dotenv requests
+pip install python-dotenv requests bs4
 ```
 
 ## 실행방법
@@ -27,4 +27,4 @@ pip install python-dotenv requests
 python main.py
 ```
 
-로그인이 성공하면 "로그인 성공"이 출력되고, 실패하면 "로그인 실패"가 출력됩니다.
+로그인이 성공하면 이름과 학번이 출력되고, 실패하면 "로그인에 실패하였습니다."가 출력됩니다.
--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 import os
 import requests
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 # .env 파일 불러오기
 load_dotenv()
 
-login_url = "https://learn.inha.ac.kr/login/index.php"
-session = requests.Session()
-
-# .env 파일에서 사용자 아이디와 비밀번호를 불러오기
+# 사용자 아이디와 비밀번호 가져오기
 username = os.getenv("INHA_USERNAME")
 password = os.getenv("INHA_PASSWORD")
 
+# 로그인 정보
 payload = {
-    "username": username,
-    "password": password,
-    "rememberusername": "1",  # 사용자 이름 기억 여부 (1 = 기억, 0 = 기억하지 않음)
+    'userid': username,
+    'password': password,
+    'loginDomain': 'inha.ac.kr',
 }
 
-# POST 요청을 사용하여 로그인
-response = session.post(login_url, data=payload)
+# 로그인 URL
+login_url = 'https://cloud.inha.ac.kr/auth/login'
 
-# 로그인 성공 여부 확인
-if response.url == login_url:
-    print("로그인 실패")
-else:
-    print("로그인 성공")
+# 세션 생성
+with requests.Session() as session:
+    # 로그인 페이지 요청
+    res = session.get(login_url)
+
+    # 로그인 요청
+    res = session.post(login_url, data=payload)
+
+    # 로그인 실패시 예외처리
+    try:
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"로그인에 실패하였습니다. 오류코드: {res.status_code}")
+        print(e)
+        exit()
+
+    # 포털 페이지 요청
+    res = session.get('https://cloud.inha.ac.kr')
+
+    # BeautifulSoup 객체 생성
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    # 로그인 사용자 정보 출력
+    try:
+        login_header = soup.select_one('.loginHeader')
+        name = login_header.string.strip().split()[0]
+        student_id = login_header['data-userid']
+        print(f"이름: {name}, 학번: {student_id}")
+    except Exception as e:
+        print("사용자 정보를 찾는 중 오류가 발생했습니다.")
+        print(e)
+        print("로그인에 실패하였습니다.")
